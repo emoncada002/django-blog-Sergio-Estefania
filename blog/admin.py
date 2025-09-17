78949846dda86b19db3ba412fba4fdd21b8e4763
emoncada002 <e.moncada002@ebc.edu.mx>
2025-09-17 16:31:16 -0600

    actualizar

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Post, Author, Tag, Comment
 
-# Register your models here.
-class PostAdmin(admin.ModelAdmin) :
+
+class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date")
     list_display = ("title", "date", "author", "comment")
     prepopulated_fields = {"slug": ("title",)}
--- my_site/settings.py
+++ my_site/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i9ko$zsq^#et!4llbwhsv0ph1lyyv)1^s#zg2j=nx_&9zq0mm+'
+SECRET_KEY = (
+    'django-insecure-i9ko$zsq^#et!4llbwhsv0ph1lyyv)1^s#zg2j=nx_'
+    '&9zq0mm+'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,19 +89,30 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
